dbc2e5d941cdb6d0cce328e549ca440997da5dc7
silvadreamer <1957915633@qq.com>
2024-01-03 10:23:07 +0800

    experiment7 21301095

--- user/src/console.rs
+++ user/src/console.rs
@@ -1,6 +1,47 @@
-use core::fmt::{self, Write};
-use super::write;
+// use core::fmt::{self, Write};
+// use super::write;
+// use super::read;
 
+// const STDOUT: usize = 1;
+// const STDIN: usize = 0;
+
+// struct Stdout;
+
+// impl Write for Stdout {
+//     fn write_str(&mut self, s: &str) -> fmt::Result {
+//         write(STDOUT, s.as_bytes());
+//         Ok(())
+//     }
+// }
+
+// pub fn print(args: fmt::Arguments) {
+//     Stdout.write_fmt(args).unwrap();
+// }
+
+// #[macro_export]
+// macro_rules! print {
+//     ($fmt: literal $(, $($arg: tt)+)?) => {
+//         $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+//     }
+// }
+
+// #[macro_export]
+// macro_rules! println {
+//     ($fmt: literal $(, $($arg: tt)+)?) => {
+//         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+//     }
+// }
+
+// pub fn getchar() -> u8 {
+//     let mut c = [0u8; 1];
+//     read(STDIN, &mut c);
+//     c[0]
+// }
+
+use core::fmt::{self, Write};
+use super::{read, write};
+
+const STDIN: usize = 0;
 const STDOUT: usize = 1;
 
 struct Stdout;
@@ -28,4 +69,10 @@ macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
+}
+
+pub fn getchar() -> u8 {
+    let mut c = [0u8; 1];
+    read(STDIN, &mut c);
+    c[0]
 }
